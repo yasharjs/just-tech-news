976f3eeb8a452b74ede4611c9f9c8d9f84625098
Yashar Js <Yashar@Icys-MacBook-Pro.local>
2022-03-07 19:52:07 -0500

    added user model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/',(req,res)=>{
     // Acess our User model and run .findAll() method
-    User.findAll()
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
@@ -15,6 +17,10 @@ router.get('/',(req,res)=>{
 // GET /api/users/1
 router.get('/:id',(req,res)=>{
     User.findOne({
+        attributes: {
+            exclude:['password']
+        }
+        ,
         where: {
             id: req.params.id
         }
