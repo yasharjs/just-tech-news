c75f21193e70c77de4a187677628cd27ebd73817
Yashar Js <Yashar@Icys-MacBook-Pro.local>
2022-03-13 01:05:44 -0500

    initial handlebar setup

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
+router.use('/',homeRoutes);
 router.use('/api',apiRoutes);
+
 
 router.use((req,res)=>{
     res.status(400).end();
